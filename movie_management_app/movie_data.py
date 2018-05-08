1909b3e467e1f06e0d96d6d946a010b827aef924
Dilli Khatiwoda <dilli@dilli.com>
2018-05-08 10:38:11 -0500

    Fixed an error in the get_movie_info function.

--- movie_management_app/movie_data.py
+++ movie_management_app/movie_data.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-def get_movie_info(name)
+def get_movie_info(name):
     apikey = os.environ['OMDB_KEY']
     movie_info = {}
     url = 'http://www.omdbapi.com/?apikey='+apikey+'&'+'t='+name
