c0803bc4368f8868c5a5c4bcf2efadc18e38879b
Dilli Khatiwoda <dilli@dilli.com>
2018-05-07 19:36:19 -0500

    Added the funtionality to test functional tests and unit tests, and added the ability to see the movie trailer before adding it to the watch list.

--- movie_management_app/urls.py
+++ movie_management_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.homepage, name = 'homepage'),
+    url(r'^$', views.homepage, name = 'homepage'),
     url(r'^popularmovies/$', views.popularMovies, name = 'popular_movies'),
     # url(r'^accounts/login/$', views.login, name = 'login'),
     # url(r'^$', views.homepage, name = 'logout'),
